457d25f385f6eb37d53e6559b8b1b5a90eabddd1
emilianoserafin <78286317+emilianoserafin@users.noreply.github.com>
2022-03-13 11:32:33 -0400

    add solutions for exercises 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let allArgs = args;
+    let result = arr;
 
+    for(let i = 0; i <= allArgs.length; i++ ){
+        for(let j = 0; j <= result.length; j++){
+            if(result[j] === allArgs[i]){
+                result.splice(j, 1)
+            }
+        }
+        
+    }
+    return result;
 };
-
+console.log(removeFromArray([1,2,3,4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
