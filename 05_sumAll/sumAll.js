08b681596f0d716a99cf3667cea5311a7ba7a130
emilianoserafin <78286317+emilianoserafin@users.noreply.github.com>
2022-03-21 16:02:38 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0){
+        return `ERROR`;
+    }
+    let nums = [...arguments].sort((a, b) => a - b);
+    let result = 0;
+    for(let i = nums[0]; i <= nums[1]; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
